8a040d4cbbd24b1a2bd03bbfc585543b79aa5a28
KKahel <kevin.labrut@gmail.com>
2022-01-11 22:10:24 +0100

    add chart.js + update color

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authentificationService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+
+    if (this.authentificationService.currentUserValue) {
+      this.router.navigate(['/home']);
+    }
+
   }
 
 }
